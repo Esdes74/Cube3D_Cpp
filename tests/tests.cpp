58ac6aeb1e7ec7af4d74cd454407fa572dd28f37
Esdes74 <115540633+Esdes74@users.noreply.github.com>
2026-04-24 22:34:57 +0200

    Add: Abstract test class global usage
    Fin de mise en place de la logique des tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,15 +2,6 @@
 #include "CliTest.hpp"
 
 int main() {
-	vector<string>	str;
-	Cli				cli;
-	CliTest			cliTest;
-
-	str.push_back("bonjour");
-	str.push_back("je");
-	str.push_back("suis");
-	str.push_back("un");
-	str.push_back("test");
-	cli.executeBonjour(str);
+	CliTest().runTest();
 	return 0;
 }
